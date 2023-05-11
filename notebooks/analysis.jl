3a91a493eae79143f27836546dcc07a35051078c
Dale Black <djblack@uci.edu>
2023-05-11 15:55:45 -0700

    run to visualization

--- notebooks/analysis.jl
+++ notebooks/analysis.jl
@@ -15,6 +15,7 @@ macro bind(def, element)
 end
 
 # ╔═╡ 5463443f-a69e-4766-bfc2-0b7ca0fd48c9
+# ╠═╡ show_logs = false
 using DrWatson; 
 
 # ╔═╡ 6495cb7b-67ec-49da-bc5d-f7a63ca9e5e9
@@ -33,6 +34,9 @@ include(srcdir("load_model.jl")), include(srcdir("postprocessing.jl"));
 
 # ╔═╡ f81ac05c-b5f2-4e96-b370-adc3b073d5c9
 include(srcdir("load_model.jl"));
+
+# ╔═╡ 20114ef8-cf6b-4a92-89b8-40a85571d6c2
+include(srcdir("postprocessing.jl"))
 
 # ╔═╡ 278dfa0e-46e1-4789-9f51-eb3463a9fb00
 TableOfContents()
@@ -326,8 +330,7 @@ end;
 
 # ╔═╡ 475618b0-885a-4fb3-90aa-57a7a649ddcb
 df_metrics_results = DataFrame(
-	"Loss Function" => ["Dice Loss", "HD-Dice Loss"
-	],
+	"Loss Function" => [labels[1], labels[3]],
 	"Dice Similarity Coefficient" => [dsc_dicemetric, hd_dicemetric],
 	"Hausdorff Distance (mm)" => [dsc_hausdorffmetric, hd_hausdorffmetric]
 )
@@ -346,16 +349,24 @@ md"""
 """
 
 # ╔═╡ 6a0ae2dd-f584-4bf2-90a0-35cbf90f9015
-data_dir = "/Users/daleblack/Library/CloudStorage/GoogleDrive-djblack@uci.edu/My Drive/Datasets/Task02_Heart"
+# const data_dir = "/Users/daleblack/Library/CloudStorage/GoogleDrive-djblack@uci.edu/My Drive/Datasets/Task02_Heart"
+const data_dir = joinpath(dirname(dirname(dirname(pwd()))), "datasets/Task02_Heart")
 
 # ╔═╡ da41cb15-042c-4f00-9430-d509c30d381b
-const model_path = "/Users/daleblack/Library/CloudStorage/GoogleDrive-djblack@uci.edu/My Drive/Datasets/hd-loss models";
+# const model_path = "/Users/daleblack/Library/CloudStorage/GoogleDrive-djblack@uci.edu/My Drive/Datasets/hd-loss models"
+const model_path = joinpath(dirname(dirname(dirname(pwd()))), "datasets/hd-loss models")
 
 # ╔═╡ c65bb13d-3f2a-4e50-a092-1a7cc2925c7e
 task2, model_dsc = loadtaskmodel(joinpath(model_path, "bigger_NN_0.001_Dice_270.jld2"))
 
 # ╔═╡ dd01154e-ceac-4fcd-9b9a-5a9268e7b2ea
 _, model_hd = loadtaskmodel(joinpath(model_path, "bigger_NN_0.001_HD_Dice_270.jld2"))
+
+# ╔═╡ a70682fa-01fe-4efa-a944-e328f546fb5b
+begin
+	model_dsc_gpu = model_dsc |> gpu
+	model_hd_gpu = model_hd |> gpu
+end;
 
 # ╔═╡ 22a5453f-72e7-4990-86ea-68ef6cc466cb
 begin
@@ -377,6 +388,7 @@ img_container, mask_container = presize(pre_data)
 data_resized = (img_container, mask_container);
 
 # ╔═╡ 023d9b3a-5fb7-4543-91b3-d2fbb2350a24
+# ╠═╡ show_logs = false
 a, b = FastVision.imagedatasetstats(img_container, Gray{N0f8})
 
 # ╔═╡ 3c4ac7bd-cc6f-42e5-abfb-0e6fba855a49
@@ -396,7 +408,7 @@ task = SupervisedTask(
 train_files, val_files = MLDataPattern.splitobs(data_resized, 0.8);
 
 # ╔═╡ 9afada46-48f4-4e22-86c3-351425e7c3ab
-batch_size = 4
+batch_size = 1
 
 # ╔═╡ d816d63d-6875-4a70-8483-e469603660b9
 tdl, vdl = FastAI.taskdataloaders(train_files, val_files, task, batch_size);
@@ -409,38 +421,39 @@ md"""
 # ╔═╡ 4325537e-47c3-45ae-a162-133eda8d03d4
 begin
 	(example,) = vdl
-    xs, ys = example
+    img, msk = example
 end;
 
-# ╔═╡ 98cebb61-5fa4-4800-a734-ca6190cca5f0
-# begin
-#     y_pred = model_hd(xs)
-#     y_pred = keep_largest_component(argmax_2ch(y_pred))
-# end;
+# ╔═╡ af635d0c-bd5c-4e93-8295-53795afbe4cf
+# ╠═╡ show_logs = false
+begin
+	pred_dsc = model_dsc_gpu(img |> gpu)
+	pred_hd = model_hd_gpu(img |> gpu)
+	
+	pred_dsc = pred_dsc |> cpu
+	pred_hd = pred_hd |> cpu
 
-# ╔═╡ 9ffc5a7b-97fc-46cc-9b2b-6aa89f060fee
-# begin
-#     y_pred_dice = model_dsc(xs)
-#     y_pred_dice = keep_largest_component(argmax_2ch(y_pred_dice))
-# end;
+	pred_dsc = keep_largest_component(argmax_2ch(pred_dsc))
+	pred_hd = keep_largest_component(argmax_2ch(pred_hd))
+end;
 
-# ╔═╡ 525e2404-9887-4347-843b-ef22c809c077
-# begin
-# 	x1, x2, x3, x4 = xs[:, :, :, :, 1], xs[:, :, :, :, 2], xs[:, :, :, :, 3], xs[:, :, :, :, 4]
-# 	x1, x2, x3, x4 = reshape(x1, size(x1)[1:end-1]), reshape(x2, size(x2)[1:end-1]), reshape(x3, size(x3)[1:end-1]), reshape(x4, size(x4)[1:end-1])
-# 	y1, y2, y3, y4 = ys[:, :, :, 2, 1], ys[:, :, :, 2, 2], ys[:, :, :, 2, 3], ys[:, :, :, 2, 4]
-# 	y1_pred, y2_pred, y3_pred, y4_pred = y_pred[:, :, :, 1], y_pred[:, :, :, 2], y_pred[:, :, :, 3], y_pred[:, :, :, 4]
-# 	y1_pred_dice, y2_pred_dice, y3_pred_dice, y4_pred_dice = y_pred_dice[:, :, :, 1], y_pred_dice[:, :, :, 2], y_pred_dice[:, :, :, 3], y_pred_dice[:, :, :, 4]
+# ╔═╡ 315e6f9b-447e-4ef4-8aaf-e74f83b31012
+img_size = (512, 512, 112)
 
-# 	img_size = (512, 512, 112)
-# 	x1, x2 = imresize(x1, img_size), imresize(x3, img_size)
-# 	y1, y2 = round.(imresize(y1, img_size)), round.(imresize(y3, img_size))
-# 	y1_pred, y2_pred = round.(imresize(y1_pred, img_size)), round.(imresize(y3_pred, img_size))
-# 	y1_pred_dice, y2_pred_dice = round.(imresize(y1_pred_dice, img_size)), round.(imresize(y3_pred_dice, img_size))
-# end;
+# ╔═╡ bc3528e9-3115-458a-a928-2e9e23033568
+begin
+	img1 = imresize(img[:, :, :, 1, 1], img_size)
+	img2 = imresize(img[:, :, :, 1, 2], img_size)
 
-# ╔═╡ 91c3eddf-57b0-419d-a37c-c799b3cc6352
-# size(x1), size(y1), size(y1_pred), size(y1_pred_dice)
+	msk1 = Bool.(round.(imresize(msk[:, :, :, 2, 1] .> 0, img_size)))
+	msk2 = Bool.(round.(imresize(msk[:, :, :, 2, 2] .> 0, img_size)))
+
+	pred_dsc1 = Bool.(round.(imresize(pred_dsc[:, :, :, 1], img_size)))
+	pred_dsc2 = Bool.(round.(imresize(pred_dsc[:, :, :, 2], img_size)))
+
+	pred_hd1 = Bool.(round.(imresize(pred_hd[:, :, :, 1], img_size)))
+	pred_hd2 = Bool.(round.(imresize(pred_hd[:, :, :, 2], img_size)))
+end;
 
 # ╔═╡ e1b32b84-998d-41e9-a7a8-a9680147f056
 md"""
@@ -448,98 +461,103 @@ md"""
 """
 
 # ╔═╡ c1d410ff-c17e-4532-b1ad-777b242b3770
-@bind a1 PlutoUI.Slider(axes(x2, 3), default=70, show_value=true)
+@bind a1 PlutoUI.Slider(axes(img, 3), default=70, show_value=true)
 
-# ╔═╡ f6a97124-dc51-437d-b352-94f46f3f4c22
+# ╔═╡ 470102a3-0aa6-4d34-a5f7-eb081e812edb
 let
-	f = Figure(;resolution=(800, 600))
+	img = img1[:, :, a1]
+	edges_gt = find_edge_idxs(msk1[:, :, a1])
+	edges_dsc = find_edge_idxs(pred_dsc1[:, :, a1])
+	edges_hd = find_edge_idxs(pred_hd1[:, :, a1])
+
+	f = Figure(; resolution=(1200, 800))
 	alpha = 1
-	markersize = 2
-	
-	y2_edge, y2_pred_edge, y2_pred_dice_edge = get_mask_edges(y2[:, :, a], y2_pred[:, :, a], y2_pred_dice[:, :, a])
-	y2_edge, y2_pred_edge, y2_pred_dice_edge = Tuple.(y2_edge), Tuple.(y2_pred_edge), Tuple.(y2_pred_dice_edge)
+	markersize = 4
 	
     ax = Axis(
 		f[1, 1],
-		title = "Dice Loss"
+		title = labels[1]
 	)
-    heatmap!(x2[:, :, a]; colormap=:grays)
-	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y2_pred_dice_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+    heatmap!(img; colormap=:grays)
+	scatter!(edges_gt; markersize=markersize, color = :red, label="Ground Truth")
+	scatter!(edges_hd; markersize=markersize, color = :blue, label="Predicted")
 	hidedecorations!(ax)
 
 	ax = Axis(
 		f[1, 2],
-		title = "HD-Dice Loss"
+		title = labels[3]
 	)
-    heatmap!(x2[:, :, a]; colormap=:grays)
-	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y2_pred_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+    heatmap!(img; colormap=:grays)
+	scatter!(edges_gt; markersize=markersize, color = :red, label="Ground Truth")
+	scatter!(edges_hd; markersize=markersize, color = :blue, label="Predicted")
 	hidedecorations!(ax)
 
-
+	axislegend(ax)
 	f
 end
 
+# ╔═╡ 784a6e9f-4a12-4b2c-be22-45e606fea872
+unique(pred_dsc1[:, :, a1] - pred_hd1[:, :, a1])
+
 # ╔═╡ 4f43c115-20eb-4041-9f8a-40286a9fd7e5
-let
-	f = Figure(
-	)
-	alpha = 1
-	markersize = 2
+# let
+# 	f = Figure(
+# 	)
+# 	alpha = 1
+# 	markersize = 2
 
-	slice_1 = 70
-	y1_edge, y1_pred_edge, y1_pred_dice_edge = get_mask_edges(y1[:, :, slice_1], y1_pred[:, :, slice_1], y1_pred_dice[:, :, slice_1])
-	y1_edge, y1_pred_edge, y1_pred_dice_edge = Tuple.(y1_edge), Tuple.(y1_pred_edge), Tuple.(y1_pred_dice_edge)
+# 	slice_1 = 70
+# 	y1_edge, y1_pred_edge, y1_pred_dice_edge = get_mask_edges(y1[:, :, slice_1], y1_pred[:, :, slice_1], y1_pred_dice[:, :, slice_1])
+# 	y1_edge, y1_pred_edge, y1_pred_dice_edge = Tuple.(y1_edge), Tuple.(y1_pred_edge), Tuple.(y1_pred_dice_edge)
 	
-    ax1 = Axis(
-		f[1, 1],
-		title = "Dice Loss"
-	)
-    heatmap!(x1[:, :, slice_1]; colormap=:grays)
-	scatter!(y1_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y1_pred_dice_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
-	hidedecorations!(ax1)
+#     ax1 = Axis(
+# 		f[1, 1],
+# 		title = "Dice Loss"
+# 	)
+#     heatmap!(x1[:, :, slice_1]; colormap=:grays)
+# 	scatter!(y1_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
+# 	scatter!(y1_pred_dice_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+# 	hidedecorations!(ax1)
 	
 
-	ax2 = Axis(
-		f[1, 2],
-		title = "HD-Dice Loss"
-	)
-    heatmap!(x1[:, :, slice_1]; colormap=:grays)
-	scatter!(y1_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y1_pred_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
-	hidedecorations!(ax2)
+# 	ax2 = Axis(
+# 		f[1, 2],
+# 		title = "HD-Dice Loss"
+# 	)
+#     heatmap!(x1[:, :, slice_1]; colormap=:grays)
+# 	scatter!(y1_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
+# 	scatter!(y1_pred_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+# 	hidedecorations!(ax2)
 
 
-	slice_2 = 43
-	y2_edge, y2_pred_edge, y2_pred_dice_edge = get_mask_edges(y2[:, :, slice_2], y2_pred[:, :, slice_2], y2_pred_dice[:, :, slice_2])
-	y2_edge, y2_pred_edge, y2_pred_dice_edge = Tuple.(y2_edge), Tuple.(y2_pred_edge), Tuple.(y2_pred_dice_edge)
+# 	slice_2 = 43
+# 	y2_edge, y2_pred_edge, y2_pred_dice_edge = get_mask_edges(y2[:, :, slice_2], y2_pred[:, :, slice_2], y2_pred_dice[:, :, slice_2])
+# 	y2_edge, y2_pred_edge, y2_pred_dice_edge = Tuple.(y2_edge), Tuple.(y2_pred_edge), Tuple.(y2_pred_dice_edge)
 
-	ax3 = Axis(
-		f[2, 1]
-	)
-    heatmap!(x2[:, :, slice_2]; colormap=:grays)
-	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y2_pred_dice_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
-	hidedecorations!(ax3)
+# 	ax3 = Axis(
+# 		f[2, 1]
+# 	)
+#     heatmap!(x2[:, :, slice_2]; colormap=:grays)
+# 	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
+# 	scatter!(y2_pred_dice_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+# 	hidedecorations!(ax3)
 	
-	ax4 = Axis(
-		f[2, 2]
-	)
-    heatmap!(x2[:, :, slice_2]; colormap=:grays)
-	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
-	scatter!(y2_pred_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
-	hidedecorations!(ax4)
+# 	ax4 = Axis(
+# 		f[2, 2]
+# 	)
+#     heatmap!(x2[:, :, slice_2]; colormap=:grays)
+# 	scatter!(y2_edge; markersize=markersize, color = (:red, alpha), label="Ground Truth")
+# 	scatter!(y2_pred_edge; markersize=markersize, color = (:blue, alpha), label="Predicted")
+# 	hidedecorations!(ax4)
 
-	Legend(f[1:2, 3],
-    [LineElement(color = :red, linestyle = nothing), LineElement(color = :blue, linestyle = nothing)],
-    ["Ground Truth", "Predicted"]; framevisible=false)
+# 	Legend(f[1:2, 3],
+#     [LineElement(color = :red, linestyle = nothing), LineElement(color = :blue, linestyle = nothing)],
+#     ["Ground Truth", "Predicted"]; framevisible=false)
 
-	save(plotsdir("contours.png"), f)
+# 	save(plotsdir("contours.png"), f)
 	
-    f
-end
+#     f
+# end
 
 # ╔═╡ Cell order:
 # ╠═5463443f-a69e-4766-bfc2-0b7ca0fd48c9
@@ -581,7 +599,7 @@ end
 # ╠═63f3eda3-5404-4053-bee3-05a6b77b950e
 # ╠═812f6634-8509-4588-a4ce-9a06e9fa8c15
 # ╠═9e5416b9-b737-4964-9baf-15d5cbdc9474
-# ╟─475618b0-885a-4fb3-90aa-57a7a649ddcb
+# ╠═475618b0-885a-4fb3-90aa-57a7a649ddcb
 # ╠═7e586270-1111-490a-849c-f41a5f55059c
 # ╟─f88c873b-469e-4a2f-b5cb-ce0bca21ca9f
 # ╟─b7338313-06bd-484f-adcd-3cb6aa836018
@@ -590,6 +608,7 @@ end
 # ╠═da41cb15-042c-4f00-9430-d509c30d381b
 # ╠═c65bb13d-3f2a-4e50-a092-1a7cc2925c7e
 # ╠═dd01154e-ceac-4fcd-9b9a-5a9268e7b2ea
+# ╠═a70682fa-01fe-4efa-a944-e328f546fb5b
 # ╠═22a5453f-72e7-4990-86ea-68ef6cc466cb
 # ╠═76091037-b59c-4f82-99f4-39202b344180
 # ╠═f67046b8-1174-4306-8ebf-422203dcfdf7
@@ -601,12 +620,13 @@ end
 # ╠═9afada46-48f4-4e22-86c3-351425e7c3ab
 # ╠═d816d63d-6875-4a70-8483-e469603660b9
 # ╟─35af3308-ab47-4c94-ab87-a6d3ca09717f
+# ╠═20114ef8-cf6b-4a92-89b8-40a85571d6c2
 # ╠═4325537e-47c3-45ae-a162-133eda8d03d4
-# ╠═98cebb61-5fa4-4800-a734-ca6190cca5f0
-# ╠═9ffc5a7b-97fc-46cc-9b2b-6aa89f060fee
-# ╠═525e2404-9887-4347-843b-ef22c809c077
-# ╠═91c3eddf-57b0-419d-a37c-c799b3cc6352
+# ╠═af635d0c-bd5c-4e93-8295-53795afbe4cf
+# ╠═315e6f9b-447e-4ef4-8aaf-e74f83b31012
+# ╠═bc3528e9-3115-458a-a928-2e9e23033568
 # ╟─e1b32b84-998d-41e9-a7a8-a9680147f056
 # ╟─c1d410ff-c17e-4532-b1ad-777b242b3770
-# ╠═f6a97124-dc51-437d-b352-94f46f3f4c22
+# ╟─470102a3-0aa6-4d34-a5f7-eb081e812edb
+# ╠═784a6e9f-4a12-4b2c-be22-45e606fea872
 # ╠═4f43c115-20eb-4041-9f8a-40286a9fd7e5
