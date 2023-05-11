e011f6ef9a4c2a4a6aa836720fdbaad36fe538c4
Dale Black <djblack@uci.edu>
2023-05-11 15:55:36 -0700

    update

--- src/postprocessing.jl
+++ src/postprocessing.jl
@@ -17,13 +17,18 @@ function keep_largest_component(array)
 	return label
 end
 
-function get_mask_edges(seg1::AbstractMatrix, seg2::AbstractMatrix, seg3::AbstractMatrix)
-	seg1, seg2, seg3 = Bool.(seg1), Bool.(seg2), Bool.(seg3)
-	
-    # Do binary erosion and use XOR to get edges
-    edges1 = erode(seg1) .⊻ seg1
-    edges2 = erode(seg2) .⊻ seg2
-    edges3 = erode(seg3) .⊻ seg3
-	
-	return findall(isone, edges1), findall(isone, edges2), findall(isone, edges3)
+function find_edge_idxs(mask)
+	edge = erode(mask) .⊻ mask
+	return Tuple.(findall(isone, edge))
 end
+
+# function get_mask_edges(seg1::AbstractMatrix, seg2::AbstractMatrix, seg3::AbstractMatrix)
+# 	seg1, seg2, seg3 = Bool.(seg1), Bool.(seg2), Bool.(seg3)
+	
+#     # Do binary erosion and use XOR to get edges
+#     edges1 = erode(seg1) .⊻ seg1
+#     edges2 = erode(seg2) .⊻ seg2
+#     edges3 = erode(seg3) .⊻ seg3
+	
+# 	return findall(isone, edges1), findall(isone, edges2), findall(isone, edges3)
+# end
